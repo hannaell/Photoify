21d73136f8e54bfa4bc5c51c5e019919a3593299
hannaell <hannaell91@gmail.com>
2018-12-12 13:12:13 +0100

    Added code for testing

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php require __DIR__.'/views/header.php'; ?>
 
-
+<article>
+    <?php if (isset(($_SESSION['user']))): ?>
+        <h2><?php echo 'Welcome, ' . $_SESSION['user']['name'] . '!'; ?></h2>
+    <?php endif; ?>
+    <h1><?php echo $config['title']; ?></h1>
+    <p>This is the home page.</p>
+</article>
 
 <?php require __DIR__.'/views/footer.php'; ?>
--- views/header.php
+++ views/header.php
@@ -10,7 +10,10 @@ require __DIR__.'/../app/autoload.php';
 
     <title><?php echo $config['title']; ?></title>
 
-
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="/assets/stylesheet/style.css">
 </head>
 <body>
+    <?php require __DIR__.'/navigation.php'; ?>
+
+    <div class="container py-5">
