d790dd8dfba9c74e8416f506619088dccf0e6ce9
hannaell <hannaell91@gmail.com>
2018-12-18 11:24:20 +0100

    Changed the session array

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php require __DIR__.'/views/header.php'; ?>
 
 <article>
-    <?php if (isset(($_SESSION['user']))): ?>
-        <h2><?php echo 'Welcome, ' . $_SESSION['user']['name'] . '!'; ?></h2>
+    <?php if (isset(($_SESSION['logedin']))): ?>
+        <h2><?php echo 'Welcome, ' . $_SESSION['logedin']['first_name'] . '!'; ?></h2>
     <?php endif; ?>
     <h1><?php echo $config['title']; ?></h1>
     <p>This is the home page.</p>
