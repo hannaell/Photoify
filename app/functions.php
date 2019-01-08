8f587e7523c6db108f0f65adb6eb72a13af5d1c3
hannaell <hannaell91@gmail.com>
2019-01-08 20:30:26 +0100

    Added a function for fetching a user by id

--- app/functions.php
+++ app/functions.php
@@ -16,3 +16,12 @@ if (!function_exists('redirect')) {
         exit;
     }
 }
+
+
+function getUserByID(int $id, object $pdo): array
+  {
+    $statement = $pdo->prepare('SELECT * FROM users WHERE id = :id');
+    $statement->bindParam(':id', $id, PDO::PARAM_STR);
+    $statement->execute();
+    return $statement->fetch(PDO::FETCH_ASSOC);
+  }
