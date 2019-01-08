f1d834c2c8a9afb6c8b9c488782a7b3a870fa721
hannaell <hannaell91@gmail.com>
2019-01-08 20:32:04 +0100

    Added code for echoing out the profile picture

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php require __DIR__.'/views/header.php'; ?>
 
 <article>
-    <?php if (isset(($_SESSION['logedin']))): ?>
-        <h2><?php echo 'Welcome, ' . $_SESSION['logedin']['first_name'] . '!'; ?></h2>
-    <?php endif; ?>
     <h1><?php echo $config['title']; ?></h1>
     <p>This is the home page.</p>
+    <?php if (isset(($_SESSION['logedin']))): ?>
+        <h2><?php echo 'Welcome, ' . $_SESSION['logedin']['first_name'] . ' ' . $_SESSION['logedin']['last_name'] .'!'; ?></h2>
+        <img class="profilePictureHome" src="/app/images/<?=$_SESSION['logedin']['profile_picture']?>" alt="BILD">
+    <?php endif; ?>
 </article>
 
 <?php require __DIR__.'/views/footer.php'; ?>
