b5d6c1fac61b5ad9d528dc229079e4ca70945291
hannaell <hannaell91@gmail.com>
2019-01-09 20:30:35 +0100

    Added classes

--- settings.php
+++ settings.php
@@ -7,74 +7,74 @@
 
     <form action="app/users/settings-app.php" method="post" enctype="multipart/form-data">
 
-        <div class="form-group">
+        <div class="profilePictureSettings">
             <label for="profilePicture">Change profile picture</label>
             <img src="/app/images/<?=$_SESSION['logedin']['profile_picture']?>" alt="BILD">
             <!-- <input  type="file" name="img" required> -->
-            <input type="file" name="img" required>
+            <input class="inputSettings" type="file" name="img" required>
         </div><!-- /form-group -->
 
-        <button type="submit" class="btn btn-primary">Save changes</button>
+        <button class="button" type="submit" class="btn btn-primary">Save changes</button>
     </form>
 
     <form action="app/users/settings-app.php" method="post">
 
-        <div class="form-group">
+        <div class="firstNameSettings">
             <label for="first_name">Change first name</label>
-            <input class="form-control" type="text" name="first_name" value="<?=$_SESSION['logedin']['first_name']?>" required>
-            <small class="form-text text-muted">Please provide the your first name.</small>
+            <input class="inputSettings" type="text" name="first_name" value="<?=$_SESSION['logedin']['first_name']?>" required>
+            <small class="">Please provide the your first name.</small>
         </div><!-- /form-group -->
 
-        <div class="form-group">
+        <div class="lastNameSettings">
             <label for="last_name">Change last name</label>
-            <input class="form-control" type="text" name="last_name" value="<?=$_SESSION['logedin']['last_name']?>" required>
-            <small class="form-text text-muted">Please provide the your last name.</small>
+            <input class="inputSettings" type="text" name="last_name" value="<?=$_SESSION['logedin']['last_name']?>" required>
+            <small class="">Please provide the your last name.</small>
         </div><!-- /form-group -->
 
-        <div class="form-group">
+        <div class="usernameSettings">
             <label for="username">Change user name</label>
-            <input class="form-control" type="text" name="username" value="<?=$_SESSION['logedin']['username']?>" required>
-            <small class="form-text text-muted">Please provide the your user name.</small>
+            <input class="inputSettings" type="text" name="username" value="<?=$_SESSION['logedin']['username']?>" required>
+            <small class="">Please provide the your user name.</small>
         </div><!-- /form-group -->
 
-        <div class="form-group">
+        <div class="bioSettings">
             <label for="biography">Update bio</label>
-            <input class="form-control" type="text" name="biography" value="<?=$_SESSION['logedin']['biography']?>" required>
-            <small class="form-text text-muted">Write something about yourself.</small>
+            <input class="inputSettings" type="text" name="biography" value="<?=$_SESSION['logedin']['biography']?>" required>
+            <small class="">Write something about yourself.</small>
         </div><!-- /form-group -->
 
-        <button type="submit" class="btn btn-primary">Save changes</button>
+        <button class="button" type="submit" class="btn btn-primary">Save changes</button>
     </form>
 
     <form action="app/users/settings-app.php" method="post">
 
-        <div class="form-group">
+        <div class="emailSettings">
             <label for="email">Change email</label>
-            <input class="form-control" type="email" name="email" value="<?=$_SESSION['logedin']['email']?>" required>
-            <small class="form-text text-muted">Please provide the your email address.</small>
+            <input class="inputSettings" type="email" name="email" value="<?=$_SESSION['logedin']['email']?>" required>
+            <small class="">Please provide the your email address.</small>
         </div><!-- /form-group -->
 
-        <div class="form-group">
+        <div class="confirmPasswordSettings">
             <label for="password">Confirm password</label>
-            <input class="form-control" type="password" name="password" required>
-            <small class="form-text text-muted">Please provide the your current password (passphrase).</small>
+            <input class="inputSettings" type="password" name="password" required>
+            <small class="">Please provide the your current password (passphrase).</small>
         </div><!-- /form-group -->
-        <button type="submit" class="btn btn-primary">Save changes</button>
+        <button class="button" type="submit" class="btn btn-primary">Save changes</button>
     </form>
 
     <form action="app/users/settings-app.php" method="post">
 
-        <div class="form-group">
+        <div class="changePasswordSettings">
             <label for="password">Change password</label>
-            <input class="form-control" type="password" name="password" required>
-            <small class="form-text text-muted">Please provide the your current password (passphrase).</small>
-            <input class="form-control" type="password" name="new_password" required>
-            <small class="form-text text-muted">Please provide the your new password (passphrase).</small>
-            <input class="form-control" type="password" name="confirm_password" required>
-            <small class="form-text text-muted">Please provide the your new password (passphrase).</small>
+            <input class="inputSettings" type="password" name="password" required>
+            <small class="">Please provide the your current password (passphrase).</small>
+            <input class="inputSettings" type="password" name="new_password" required>
+            <small class="">Please provide the your new password (passphrase).</small>
+            <input class="inputSettings" type="password" name="confirm_password" required>
+            <small class="">Please provide the your new password (passphrase).</small>
         </div><!-- /form-group -->
 
-        <button type="submit" class="btn btn-primary">Save changes</button>
+        <button class="button" type="submit" class="btn btn-primary">Save changes</button>
     </form>
 
 </article>
