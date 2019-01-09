a1b0a571087b4738ca5727294f263de776113b17
hannaell <hannaell91@gmail.com>
2019-01-09 16:34:13 +0100

    Added code for a like button

---
+++ app/posts/likes-posts.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+require __DIR__.'/../autoload.php';
+
+if (isset($_POST['post_id'], $_POST['action'])) {
+
+    $action = $_POST['action'];
+    $postId = $_POST['post_id'];
+    $userId = $_SESSION['logedin']['id'];
+
+    if ($action === 'disliked') {
+        $statement = $pdo->query("DELETE FROM likes WHERE post_id = '$postId' AND user_id = '$userId';");
+
+        if (!$statement) {
+            die(var_dump($pdo->errorInfo()));
+        }
+    } elseif ($action === 'liked') {
+        $statement = $pdo->prepare('INSERT INTO likes (user_id, post_id) VALUES (:user_id, :post_id);');
+
+        if (!$statement) {
+            die(var_dump($pdo->errorInfo()));
+        }
+
+        $statement->bindParam(':user_id', $userId, PDO::PARAM_INT);
+        $statement->bindParam(':post_id', $postId, PDO::PARAM_INT);
+        $statement->execute();
+    }
+
+    $statement = $pdo->query("SELECT COUNT(*) AS likes FROM likes WHERE post_id = '$postId';");
+
+    if (!$statement) {
+        die(var_dump($pdo->errorInfo()));
+    }
+
+    $likes = $statement->fetch(PDO::FETCH_ASSOC);
+    $likes = json_encode($likes);
+
+    header('Content-Type: application/json');
+    echo $likes;
+    }
+    else {
+    exit();
+}
