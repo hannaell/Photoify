cf2904c2e1bf7c60f49f71ce6ebcfd5293983b88
hannaell <hannaell91@gmail.com>
2019-01-09 20:36:41 +0100

    Added classes

--- posts.php
+++ posts.php
@@ -1,23 +1,29 @@
 <?php require __DIR__.'/views/header.php'; ?>
 
-<article>
-    <h1>Upload photo</h1>
+<?php if (isset($_SESSION['logedin'])): ?>
 
-    <form action="app/posts/posts-posts.php" method="post" enctype="multipart/form-data">
+    <article>
+        <h1>Upload photo</h1>
 
-        <div class="formFilePosts">
-            <label for="profilePicture">Upload photo</label>
-            <img class="uploadPhoto" src="/app/images/<?=$_SESSION['logedin']['content']?>" alt="BILD">
-            <input class="formFileInputPosts"type="file" name="img" required>
-        </div><!-- /form-group -->
-        <div class="formDescriptionPosts">
-            <label for="description">Picture description</label>
-            <input class="formDescriptionInputPosts" type="text" name="description" placeholder="Describe your photo..." required>
+        <form action="app/posts/posts-posts.php" method="post" enctype="multipart/form-data">
 
-        </div><!-- /form-group -->
+            <div class="filePosts">
+                <label for="profilePicture">Upload photo</label>
+                <img class="uploadPhoto" src="/app/images/<?=$_SESSION['logedin']['content']?>" alt="BILD">
+                <input class="inputPosts"type="file" name="img" required>
+            </div><!-- /form-group -->
+            <div class="descriptionPosts">
+                <label for="description">Picture description</label>
+                <input class="inputPosts" type="text" name="description" placeholder="Describe your photo...">
 
-        <button class="button" type="submit" class="btn btn-primary">Upload</button>
-    </form>
-</article>
+            </div><!-- /form-group -->
+
+            <button class="button" type="submit">Upload</button>
+        </form>
+    </article>
+
+<?php else: redirect('/login.php');?>
+
+<?php endif; ?>
 
 <?php require __DIR__.'/views/footer.php'; ?>
