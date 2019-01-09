7aaae872cf49131eb26d136edabd4e8de188cfc6
hannaell <hannaell91@gmail.com>
2019-01-09 16:38:03 +0100

    Added classes

--- posts.php
+++ posts.php
@@ -1,22 +1,22 @@
 <?php require __DIR__.'/views/header.php'; ?>
 
 <article>
-    <h1>Login</h1>
+    <h1>Upload photo</h1>
 
     <form action="app/posts/posts-posts.php" method="post" enctype="multipart/form-data">
 
-        <div class="form-group">
+        <div class="formFilePosts">
             <label for="profilePicture">Upload photo</label>
-            <img src="/app/images/<?=$_SESSION['logedin']['content']?>" alt="BILD">
-            <input type="file" name="img" required>
+            <img class="uploadPhoto" src="/app/images/<?=$_SESSION['logedin']['content']?>" alt="BILD">
+            <input class="formFileInputPosts"type="file" name="img" required>
         </div><!-- /form-group -->
-        <div class="form-group">
+        <div class="formDescriptionPosts">
             <label for="description">Picture description</label>
-            <input class="form-control" type="text" name="content" value="Describe your photo..." required>
+            <input class="formDescriptionInputPosts" type="text" name="description" placeholder="Describe your photo..." required>
 
         </div><!-- /form-group -->
 
-        <button type="submit" class="btn btn-primary">Upload</button>
+        <button class="button" type="submit" class="btn btn-primary">Upload</button>
     </form>
 </article>
 
