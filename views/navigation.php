ba0244217852df680a472b065707b9d3c3bb7e80
hannaell <hannaell91@gmail.com>
2019-01-09 20:31:34 +0100

    Added checks for session

--- views/navigation.php
+++ views/navigation.php
@@ -1,32 +1,38 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#"><?php echo $config['title']; ?></a>
+<nav class="">
+  <a class="" href="#"><?php echo $config['title']; ?></a>
 
-  <ul class="navbar-nav">
-      <li class="nav-item">
-          <a class="nav-link" href="/index.php">Home</a>
-      </li><!-- /nav-item -->
-
-      <li class="nav-item">
-          <a class="nav-link" href="/posts.php">Upload photo</a>
-      </li><!-- /nav-item -->
-
-      <li class="nav-item">
-          <a class="nav-link" href="/feed.php">News Feed</a>
-      </li><!-- /nav-item -->
-
-      <li class="nav-item">
+  <ul class="">
+      <li class="">
           <?php if (isset($_SESSION['logedin'])): ?>
-             <a class="nav-link" href="/app/users/logout-app.php">Log Out</a>
-             <?php else:  ?>
-                 <a class="nav-link" href="/login.php">Log In</a>
+              <a class="" href="/index.php">Home</a>
           <?php endif; ?>
       </li><!-- /nav-item -->
 
-      <li class="nav-item">
+      <li class="">
           <?php if (isset($_SESSION['logedin'])): ?>
-             <a class="nav-link" href="/settings.php">Settings</a>
+              <a class="" href="/posts.php">Upload photo</a>
+          <?php endif; ?>
+      </li><!-- /nav-item -->
+
+      <li class="">
+          <?php if (isset($_SESSION['logedin'])): ?>
+              <a class="" href="/feed.php">News Feed</a>
+          <?php endif; ?>
+      </li><!-- /nav-item -->
+
+      <li class="">
+          <?php if (isset($_SESSION['logedin'])): ?>
+             <a class="" href="/app/users/logout-app.php">Log Out</a>
              <?php else:  ?>
-                 <a class="nav-link" href="/register.php">Register</a>
+                 <a class="" href="/login.php">Log In</a>
+          <?php endif; ?>
+      </li><!-- /nav-item -->
+
+      <li class="">
+          <?php if (isset($_SESSION['logedin'])): ?>
+             <a class="" href="/settings.php">Settings</a>
+             <?php else:  ?>
+                 <a class="" href="/register.php">Register</a>
           <?php endif; ?>
       </li><!-- /nav-item -->
   </ul><!-- /navbar-nav -->
