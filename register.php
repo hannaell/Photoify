0b807d4a8834f426e6392e2130067214222d6ee4
hannaell <hannaell91@gmail.com>
2019-01-09 20:32:57 +0100

    Added classes

--- register.php
+++ register.php
@@ -1,48 +1,52 @@
 <?php require __DIR__.'/views/header.php'; ?>
 
 <article>
-    <h1>Register</h1>
+    <div class="register">
 
-    <form action="app/users/register-app.php" method="post">
+        <h1>Register</h1>
 
-        <div class="form-group">
-            <label for="first_name">First Name</label>
-            <input class="form-control" type="text" name="first_name" placeholder="First Name" required>
-            <small class="form-text text-muted">Please provide the your first name.</small>
-        </div><!-- /form-group -->
+        <form action="app/users/register-app.php" method="post">
 
-        <div class="form-group">
-            <label for="last_name">Last Name</label>
-            <input class="form-control" type="text" name="last_name" placeholder="Last Name" required>
-            <small class="form-text text-muted">Please provide the your last name.</small>
-        </div><!-- /form-group -->
+            <div class="firstNameRegister">
+                <label for="first_name">First Name</label>
+                <input class="inputRegister" type="text" name="first_name" placeholder="First Name" required>
+                <small class="">Please provide the your first name.</small>
+            </div><!-- /form-group -->
 
-        <div class="form-group">
-            <label for="username">User Name</label>
-            <input class="form-control" type="text" name="username" placeholder="User Name" required>
-            <small class="form-text text-muted">Please provide the your user name.</small>
-        </div><!-- /form-group -->
+            <div class="lastNameRegister">
+                <label for="last_name">Last Name</label>
+                <input class="inputRegister" type="text" name="last_name" placeholder="Last Name" required>
+                <small class="">Please provide the your last name.</small>
+            </div><!-- /form-group -->
 
-        <div class="form-group">
-            <label for="email">Email</label>
-            <input class="form-control" type="email" name="email" placeholder="francis@darjeeling.com" required>
-            <small class="form-text text-muted">Please provide the your email address.</small>
-        </div><!-- /form-group -->
+            <div class="usernameRegister">
+                <label for="username">User Name</label>
+                <input class="inputRegister" type="text" name="username" placeholder="User Name" required>
+                <small class="">Please provide the your user name.</small>
+            </div><!-- /form-group -->
 
-        <div class="form-group">
-            <label for="password">Password</label>
-            <input class="form-control" type="password" name="password" required>
-            <small class="form-text text-muted">Please provide the your password (passphrase).</small>
-        </div><!-- /form-group -->
+            <div class="emailRegister">
+                <label for="email">Email</label>
+                <input class="inputRegister" type="email" name="email" placeholder="francis@darjeeling.com" required>
+                <small class="">Please provide the your email address.</small>
+            </div><!-- /form-group -->
 
-        <div class="form-group">
-            <label for="password">Confirm Password</label>
-            <input class="form-control" type="password" name="confirm_password" required>
-            <small class="form-text text-muted">Please confirm password (passphrase).</small>
-        </div><!-- /form-group -->
+            <div class="passwordRegister">
+                <label for="password">Password</label>
+                <input class="inputRegister" type="password" name="password" required>
+                <small class="">Please provide the your password (passphrase).</small>
+            </div><!-- /form-group -->
 
-        <button type="submit" class="btn btn-primary">Register</button>
-    </form>
+            <div class="confirmPasswordRegister">
+                <label for="password">Confirm Password</label>
+                <input class="inputRegister" type="password" name="confirm_password" required>
+                <small class="">Please confirm password (passphrase).</small>
+            </div><!-- /form-group -->
+
+            <button class="button" type="submit" class="btn btn-primary">Register</button>
+        </form>
+
+    </div>
 </article>
 
 <?php require __DIR__.'/views/footer.php'; ?>
