e29faf43a6bd0afa2c992dff82fa6e9244d996bd
hannaell <hannaell91@gmail.com>
2019-01-12 13:33:43 +0100

    Fixed a bug

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 
 <article>
     <h1><?php echo $config['title']; ?></h1>
-    <p>This is the home page.</p>
+
     <?php if (isset(($_SESSION['logedin']))): ?>
-        <h2><?php echo 'Welcome, ' . $_SESSION['logedin']['first_name'] . ' ' . $_SESSION['logedin']['last_name'] .'!'; ?></h2>
+        <h2><?php echo$_SESSION['logedin']['username']; ?></h2>
         <img class="profilePictureHome" src="/app/images/<?=$_SESSION['logedin']['profile_picture']?>" alt="BILD">
         <h4><?php echo $_SESSION['logedin']['biography']; ?></h4>
     <?php endif; ?>
