04fcb4006de6be7cb1e85651363a1782e95d481a
hannaell <hannaell91@gmail.com>
2019-01-12 19:14:54 +0100

    Fixed a spelling error

--- register.php
+++ register.php
@@ -3,32 +3,28 @@
 <!-- <article> -->
     <div class="register">
 
-        <h1>Register</h1>
+        <h1>Create account</h1>
 
         <form action="app/users/register-app.php" method="post">
 
             <div class="firstNameRegister">
                 <label for="first_name">First Name</label>
                 <input class="inputRegister" type="text" name="first_name" placeholder="First Name" required>
-                <small class="">Please provide the your first name.</small>
             </div><!-- /form-group -->
 
             <div class="lastNameRegister">
                 <label for="last_name">Last Name</label>
                 <input class="inputRegister" type="text" name="last_name" placeholder="Last Name" required>
-                <small class="">Please provide the your last name.</small>
             </div><!-- /form-group -->
 
             <div class="usernameRegister">
                 <label for="username">User Name</label>
                 <input class="inputRegister" type="text" name="username" placeholder="User Name" required>
-                <small class="">Please provide the your user name.</small>
             </div><!-- /form-group -->
 
             <div class="emailRegister">
                 <label for="email">Email</label>
                 <input class="inputRegister" type="email" name="email" placeholder="francis@darjeeling.com" required>
-                <small class="">Please provide the your email address.</small>
             </div><!-- /form-group -->
 
             <div class="passwordRegister">
@@ -44,7 +40,7 @@
             </div><!-- /form-group -->
 
             <div class="buttonRegister">
-                <button class="button" type="submit">Creare account</button>
+                <button class="button" type="submit">Create account</button>
             </div>
         </form>
 
