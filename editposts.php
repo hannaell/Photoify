6b0c3377f25606940ec9a13fa408487f6d3e3c98
hannaell <hannaell91@gmail.com>
2019-01-14 15:37:20 +0100

    Removed delete button

--- editposts.php
+++ editposts.php
@@ -3,26 +3,26 @@
 require __DIR__.'/app/posts/editposts-posts.php'
 
 ?>
+<div class="editposts">
+    <h2>Edit post</h2>
 
-<img class="photoFeed" src="/app/images/<?php echo $post['content']; ?>" alt="Image">
+    <img class="photoFeed" src="/app/images/<?php echo $post['content']; ?>" alt="Image">
 
-<p>Edit post</p>
-<form action="/editposts.php?post_id=<?= $postId?>" method="post" enctype="multipart/form-data">
 
-    <div class="descriptionEditposts">
-        <label for="description">Picture description</label>
-        <input class="inputEditposts" type="text" name="description" placeholder="Describe your photo...">
-    </div><!-- /form-group -->
+    <form action="/editposts.php?post_id=<?= $postId?>" method="post" enctype="multipart/form-data">
 
-    <div class="buttonPosts">
-        <button class="button" type="submit">Save changes</button>
-    </div>
-</form>
+        <div class="descriptionEditposts">
+            <label for="description">Picture description</label>
+            <input class="inputEditposts" type="text" name="description" placeholder="Describe your photo...">
+        </div>
 
-<form action="/editposts.php?post_id=<?= $postId?>" method="post" enctype="multipart/form-data">
-    <div class="buttonPosts">
-        <button class="button" type="submit" name="delete">Delete post</button>
-    </div>
-</form>
+        <div class="buttonEditposts">
+            <button class="button" type="submit">Save changes</button>
+        </div>
+    </form>
+
+
+
+</div>
 
 <?php require __DIR__.'/views/footer.php'; ?>
