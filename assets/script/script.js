f2ae6712e76621cc87708885b80eb59a8f430231
hannaell <hannaell91@gmail.com>
2019-01-15 12:39:24 +0100

    Function for preview a photo before uploading

--- assets/script/script.js
+++ assets/script/script.js
@@ -24,12 +24,16 @@ posts.forEach(post => {
         if (json.action === "liked") {
           likeForm.querySelector("i").classList.remove("far");
           likeForm.querySelector("i").classList.add("fas");
+          likeForm.querySelector("i").classList.remove("hartIconFeed");
+          likeForm.querySelector("i").classList.add("redHart");
 
           numberOfLikes.querySelector(".likeCounterFeed").innerText =
             json.likeCount;
         } else {
           likeForm.querySelector("i").classList.remove("fas");
           likeForm.querySelector("i").classList.add("far");
+          likeForm.querySelector("i").classList.remove("redHart");
+          likeForm.querySelector("i").classList.add("hartIconFeed");
 
           numberOfLikes.querySelector(".likeCounterFeed").innerText =
             json.likeCount;
@@ -47,3 +51,22 @@ if (document.querySelector("#myDropdown")) {
     editPost.classList.toggle("show");
   });
 }
+
+//Saving input and img elements from document
+const input = document.querySelector(".inputUpload");
+const preview = document.querySelector(".uploadPhoto");
+
+//Function for putting preview of chosen picture on the page
+const showPreview = input => {
+  if (input.files && input.files[0]) {
+    let reader = new FileReader();
+    reader.onload = e => {
+      preview.src = e.target.result;
+    };
+    reader.readAsDataURL(input.files[0]);
+  }
+};
+//Waiting for a image to be chosen in input element
+input.addEventListener("input", () => {
+  showPreview(input);
+});
--- posts.php
+++ posts.php
@@ -1,4 +1,8 @@
-<?php require __DIR__.'/views/header.php'; ?>
+<?php
+
+declare(strict_types=1);
+
+require __DIR__.'/views/header.php'; ?>
 
 <?php if (isset($_SESSION['logedin'])): ?>
 
@@ -9,15 +13,15 @@
             <form action="app/posts/posts-posts.php" method="post" enctype="multipart/form-data">
 
                 <div class="filePosts">
-                    <img class="uploadPhoto" src="/app/images/<?=$_SESSION['logedin']['content']?>" alt="BILD">
-                    <input class="inputUpload"type="file" name="img" required>
+                    <img class="uploadPhoto" src="" alt="BILD">
+                    <input class="inputUpload" type="file" name="img" required>
                 </div><!-- /form-group -->
                 <div class="descriptionPosts">
                     <label for="description">Picture description</label>
                     <input class="inputPosts" type="text" name="description" placeholder="Describe your photo...">
                 </div><!-- /form-group -->
 
-                <div class="buttonPosts">
+                <div class="buttonUploadpost">
                     <button class="button" type="submit">Upload</button>
                 </div>
             </form>
