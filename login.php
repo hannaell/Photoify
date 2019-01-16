bb82c05bb22b1b3180989b6de2dfc69b64868462
hannaell <hannaell91@gmail.com>
2019-01-16 13:00:35 +0100

    CSS done

--- login.php
+++ login.php
@@ -1,29 +1,31 @@
 <?php require __DIR__.'/views/header.php'; ?>
 
-<!-- <article> -->
+
 <div class="login">
+    <h2 class="h2Login">Login</h2>
+    <div class="containerLogin">
+        <form action="app/users/login-app.php" method="post">
+            <div class="emailLogin">
+                <label for="email">Email</label>
+                <input class="inputLogin" type="email" name="email" placeholder="francis@darjeeling.com" required>
+            </div><!-- /form-group -->
 
-    <h1><?php echo $config['title']; ?></h1>
-    <h2>Login</h2>
+            <div class="passwordLogin">
+                <label for="password">Password</label>
+                <input class="inputLogin" type="password" name="password" required>
+            </div><!-- /form-group -->
 
-    <form action="app/users/login-app.php" method="post">
-        <div class="emailLogin">
-            <label for="email">Email</label>
-            <input class="inputLogin" type="email" name="email" placeholder="francis@darjeeling.com" required>
-        </div><!-- /form-group -->
+            <div class="buttonLogin">
+                <button class="button" type="submit">Login</button>
+            </div>
 
-        <div class="passwordLogin">
-            <label for="password">Password</label>
-            <input class="inputLogin" type="password" name="password" required>
-        </div><!-- /form-group -->
-
-        <div class="buttonLogin">
-            <button class="button" type="submit">Login</button>
-        </div>
-
-    </form>
-
+        </form>
+    </div>
+    <div class="createLogin">
+        <p>Don't have an account?</p>
+        <a href="/register.php">Create account</a>
+    </div>
 </div>
-<!-- </article> -->
+
 
 <?php require __DIR__.'/views/footer.php'; ?>
