1dec6fbde3acae57cc0631d783f36e14bbce4b23
hannaell <hannaell91@gmail.com>
2019-01-16 12:43:18 +0100

    Added redirect

--- index.php
+++ index.php
@@ -1,13 +1,16 @@
-<?php require __DIR__.'/views/header.php'; ?>
+<?php
 
-<article>
-    <h1><?php echo $config['title']; ?></h1>
+declare(strict_types=1);
 
-    <?php if (isset(($_SESSION['logedin']))): ?>
-        <h2><?php echo$_SESSION['logedin']['username']; ?></h2>
-        <img class="profilePictureHome" src="/app/images/<?=$_SESSION['logedin']['profile_picture']?>" alt="BILD">
-        <h4><?php echo $_SESSION['logedin']['biography']; ?></h4>
-    <?php endif; ?>
-</article>
+require __DIR__.'/views/header.php';
 
-<?php require __DIR__.'/views/footer.php'; ?>
+if (isset($_SESSION['logedin'])) {
+    redirect('/feed.php');
+}
+else {
+    redirect('/login.php');
+}
+
+require __DIR__.'/views/footer.php';
+
+?>
